e5451251604878135e0befeb6b95c0c25cae2554
WMXPY <wm@mengw.io>
2021-11-02 12:01:47 -0500

    add empty return symbol

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,5 @@
  */
 
 export * from "./symbol/invalid";
+export * from "./symbol/return";
 
---
+++ src/symbol/return.ts
@@ -0,0 +1,8 @@
+/**
+ * @author WMXPY
+ * @namespace Symbol
+ * @description Return
+ */
+
+export const EmptyReturnSymbolText: string = 'empty-return';
+export const EmptyReturnSymbol: unique symbol = Symbol.for(EmptyReturnSymbolText);
